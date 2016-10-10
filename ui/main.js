cff5c4063b9f999defd262dc26600f6db025d5b8
ckarthikk <karthik87.ck@gmail.com>
2016-10-10 16:49:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+//Change the Main Text
+var element = document.getElementByID("mainText");
+element.innerHTML = "New Text";
+
+//Move the Image
+var imgElement = document.getElementByID("madi");
+imgElement.onClick = function(){
+    var interval = setInterval(moveRight, 100);
+}
+
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    imgElement.style.marginLeft = marginLeft + "px";
+}
