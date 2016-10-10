a020a91a129e171cc3604a71c6c0b072364ca0bf
ckarthikk <karthik87.ck@gmail.com>
2016-10-10 10:56:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articleone', function (req, res) {
-  res.send("This is Article One");
+  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
