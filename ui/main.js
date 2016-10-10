50fd2a5711dfd439d2656b06184d2301c5c188c3
ckarthikk <karthik87.ck@gmail.com>
2016-10-10 16:56:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('mainText');
 element.innerHTML = "New Text";
 
 //Move the Image
-var imgElement = document.getElementById("madi");
+var imgElement = document.getElementById('madi');
 imgElement.onClick = function(){
     var interval = setInterval(moveRight, 100);
 };
@@ -13,5 +13,5 @@ imgElement.onClick = function(){
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    imgElement.style.marginLeft = marginLeft + "px";
+    imgElement.style.marginLeft = marginLeft + 'px';
 }
